4294a054bb14093a1b1b3a965bc43526e04a2c51
shiw <shiw@benqio.com>
2022-11-25 17:46:11 +0800

    update

--- review01/hello.c
+++ review01/hello.c
@@ -1,10 +1,26 @@
 //
 // Created by shiwei on 2022/11/25.
 //
-
+/**
+ * 变量申明及输入占位符
+ */
 #include <stdio.h>
 
 int main(){
-    printf("hello");
+    int num = 10;
+    printf("num变量的值%d\n",num);
+
+    double a = 1.22;
+    printf("a变量的值%f\n",a);
+
+    float b = 3.2323f;
+    printf("b变量的值%lf\n",b);
+
+    char c = 'a'; //并且单引号修饰
+    printf("c变量的值%c\n",c);
+
+
+
+
     return 0;
 }
